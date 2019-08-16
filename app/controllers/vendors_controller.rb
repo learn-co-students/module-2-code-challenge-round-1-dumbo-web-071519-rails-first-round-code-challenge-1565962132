67764bded8ba45ce3cabb3ae3b10594a4a668bc9
Ashna Shah-Grover <as19@williams.edu>
2019-08-16 11:08:07 -0400

    Completed lab

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,7 +1,19 @@
 class VendorsController < ApplicationController
 
+  before_action :find_vendor, only: [:show]
+
   def index
     @vendors = Vendor.all
+  end
+
+  def show
+    @vendor_sweet = VendorSweet.find_by(vendor_id: @vendor)
+  end
+
+  private
+
+  def find_vendor
+    @vendor = Vendor.find(params[:id])
   end
 
 end
