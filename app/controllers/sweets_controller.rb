6757bb2f3515c137a7b62966ca9e59419b1a4ada
Janet Mendez <janetmendez.03@gmail.com>
2019-08-16 11:08:24 -0400

    Done~

--- app/controllers/sweets_controller.rb
+++ app/controllers/sweets_controller.rb
@@ -2,4 +2,11 @@ class SweetsController < ApplicationController
   def index
     @sweets = Sweet.all
   end
+  
+  def show
+    @sweet = Sweet.find(params[:id])
+    #This is an array of vendor instances that are associated with that sweet
+    #Again, these are instances~
+    @vendors = @sweet.vendors.uniq
+  end
 end
--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,7 +1,12 @@
 class VendorsController < ApplicationController
-
   def index
     @vendors = Vendor.all
   end
 
+  def show
+    @vendor = Vendor.find(params[:id])
+    #This is all of the VENDOR SWEETS instances that are associated with that vendor
+    #These are instances~
+    @vsweets = @vendor.vendor_sweets
+  end
 end
